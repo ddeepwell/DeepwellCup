d5808581fb6d25df0daaab5108eeed0a12d4ce21
David Deepwell <ddeepwel@uwaterloo.ca>
2022-06-09 21:23:04 -0600

    Add functionality for plotting with OtherPoints table
    Also improve legend specification.

--- scripts/plots.py
+++ scripts/plots.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from scripts.scores import year_points_table
+from scripts.database import DataBaseOperations
 
 # set font to look like Latex
 font = {'family' : 'serif',
@@ -20,6 +21,11 @@ def year_chart(year, max_round='Champions', save=False):
 
     points_unsorted_df = year_points_table(year)
     round_names = points_unsorted_df.index[:-1].tolist()
+    db_ops = DataBaseOperations()
+    other_data = []
+    with db_ops as db:
+        for rnd in [1,2,3,4]:
+            other_data.append(db.get_other_points(year, rnd))
 
     # parse which rounds to plot
     # In some years, after the third round you some individuals
@@ -64,18 +70,24 @@ def year_chart(year, max_round='Champions', save=False):
                     )
                 )
                 # add text
-                patch = axis_list[-1][0]
-                left_patch_position, bottom_patch_position = patch.get_xy()
-                x_round = 0.5*patch.get_width() + left_patch_position
-                y_individual = 0.5*patch.get_height() + bottom_patch_position
-                # if round_points != 0:
-                axis.text(x_round, y_individual, f'{round_points}', ha='center', va='center')
+                y_individual = add_point_string(
+                    individual, playoff_round, round_points, other_data, axis, axis_list)
                 left += round_points
             if playoff_round == rounds_to_plot[-1]:
                 # add total sum of points outside bar graph
                 x_total = left + max_points/25
                 total_points = points_df[individual].loc[rounds_to_plot].sum()
                 axis.text(x_total, y_individual, f'{total_points}', ha='center',va='center')
+            if playoff_round == 'Champions' and len(axis_list) == 4:
+                axis_list.append(
+                    axis.barh(individual_i, 0,
+                                left = left,
+                                align = 'center',
+                                edgecolor = 'black',
+                                color = colors[playoff_round],
+                                label = playoff_round
+                    )
+                )
 
     # set axis and add labels
     y_pos = np.arange(num_individuals)
@@ -97,18 +109,51 @@ def year_chart(year, max_round='Champions', save=False):
     axis.xaxis.set_ticks_position('none')
     axis.get_xaxis().set_ticks([])
 
-    add_legend(axis_list)
+    add_legend(axis_list, other_data)
     if save:
         # save figure for year
         file_name = fig_title.replace(' ','')
         save_figure(file_name, year)
 
 
-def add_legend(axis_list):
+def add_point_string(individual, playoff_round, round_points, other_data, axis, axis_list):
+    '''Add string of the points earned by the individual in a playoff round'''
+
+    # find position to place the string
+    patch = axis_list[-1][0]
+    left_patch_position, bottom_patch_position = patch.get_xy()
+    x_round = 0.5*patch.get_width() + left_patch_position
+    y_individual = 0.5*patch.get_height() + bottom_patch_position
+
+    # create the string
+    point_string = f'{round_points}'
+    round_number = int(playoff_round[-1]) if playoff_round[:5] == 'Round' else None
+    if round_number in [1,2,3,4]:
+        if individual in other_data[round_number-1].index:
+            point_string += '$\\ast$'
+
+    axis.text(x_round, y_individual,
+        point_string,
+        ha='center', va='center')
+
+    return y_individual
+
+def add_legend(axis_list, other_data):
     '''Add a legend'''
 
     # remove unused elements of a_bar
     a_bar2 = list(filter(lambda a: a != 0, axis_list))
+    last_index = len(axis_list)
+    add_star_description = sum([len(other.index) for other in other_data[:last_index]]) != 0
+    if add_star_description:
+        a_bar2.append(
+            mpl.patches.Rectangle((0, 0), 1, 1,
+                facecolor = "none",
+                fill = False,
+                edgecolor = 'none',
+                linewidth = 0,
+                label = '$\\ast$: -7 points')
+        )
 
     # Put a legend to the right of the current axis
     plt.legend(handles=a_bar2, loc='center left', bbox_to_anchor=(1.05,0.5))
