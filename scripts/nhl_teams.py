0665c32750d0a884fff1bb03c967865a22b6ef6d
David Deepwell <ddeepwel@uwaterloo.ca>
2022-09-24 20:01:36 -0600

    Fix conference lists

--- scripts/nhl_teams.py
+++ scripts/nhl_teams.py
@@ -148,43 +148,43 @@ def conference(team, year):
     "Return the conference that a team belongs in"
 
     east_list = [
-        'BOS', 'Boston Bruins'
-        'BUF', 'Buffalo Sabres'
-        'CAR', 'Carolina Hurricanes'
-        'FLA', 'Florida Panthers'
-        'MTL', 'Montreal Canadiens'
-        'NJD', 'New Jersey Devils'
-        'NYI', 'New York Islanders'
-        'NYR', 'New York Rangers'
-        'OTT', 'Ottawa Senators'
-        'PHI', 'Philadelphia Flyers'
-        'PIT', 'Pittsburgh Penguins'
-        'TBL', 'Tampa Bay Lightning'
-        'TOR', 'Toronto Maple Leafs'
-        'WSH', 'Washington Capitals'
+        'BOS', 'Boston Bruins',
+        'BUF', 'Buffalo Sabres',
+        'CAR', 'Carolina Hurricanes',
+        'FLA', 'Florida Panthers',
+        'MTL', 'Montreal Canadiens',
+        'NJD', 'New Jersey Devils',
+        'NYI', 'New York Islanders',
+        'NYR', 'New York Rangers',
+        'OTT', 'Ottawa Senators',
+        'PHI', 'Philadelphia Flyers',
+        'PIT', 'Pittsburgh Penguins',
+        'TBL', 'Tampa Bay Lightning',
+        'TOR', 'Toronto Maple Leafs',
+        'WSH', 'Washington Capitals',
     ]
     west_list = [
-        'ANA', 'Anaheim Ducks'
-        'ARI', 'Arizona Coyotes', 'Phoenix Coyotes'
-        'CGY', 'Calgary Flames'
-        'COL', 'Colorado Avalanche'
-        'DAL', 'Dallas Stars'
-        'EDM', 'Edmonton Oilers'
-        'LAK', 'Los Angeles Kings'
-        'MIN', 'Minnesota Wild'
-        'SJS', 'San Jose Sharks'
-        'STL', 'St Louis Blues'
-        'VAN', 'Vancouver Canucks'
-        'VGK', 'Vegas Golden Knights'
-        'NSH', 'Nashville Predators'
-        'CHI', 'Chicago Blackhawks'
-        'SEA', 'Seattle Kraken'
+        'ANA', 'Anaheim Ducks',
+        'ARI', 'Arizona Coyotes', 'Phoenix Coyotes',
+        'CGY', 'Calgary Flames',
+        'COL', 'Colorado Avalanche',
+        'DAL', 'Dallas Stars',
+        'EDM', 'Edmonton Oilers',
+        'LAK', 'Los Angeles Kings',
+        'MIN', 'Minnesota Wild',
+        'SJS', 'San Jose Sharks',
+        'STL', 'St Louis Blues',
+        'VAN', 'Vancouver Canucks',
+        'VGK', 'Vegas Golden Knights',
+        'NSH', 'Nashville Predators',
+        'CHI', 'Chicago Blackhawks',
+        'SEA', 'Seattle Kraken',
     ]
 
     if year <= 2013:
         west_list += [
-            'CBJ', 'Columbus Blue Jackets'
-            'DET', 'Detroit Red Wings'
+            'CBJ', 'Columbus Blue Jackets',
+            'DET', 'Detroit Red Wings',
         ]
         east_list += ['WPG', 'Winnipeg Jets']
     if year > 2013:
