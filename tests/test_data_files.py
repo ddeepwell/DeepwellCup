0e0b9229a190aa150716d275209a9345f993e232
David Deepwell <ddeepwel@uwaterloo.ca>
2022-12-01 19:38:51 -0700

    Reorganize test_data_files to use pytest

--- tests/test_data_files.py
+++ tests/test_data_files.py
@@ -1,79 +1,71 @@
 """Tests for data_file"""
 import os
 from pathlib import Path
-from unittest import TestCase
+import pytest
 from scripts import DataFile
 
-class DataFileTest(TestCase):
-    """ Class for tests of the DataFile class"""
-
-    def setUp(self):
-        """ General setup options"""
-
+class Settings:
+    """Test settings"""
+    def __init__(self):
         self.tests_dir = Path(os.path.dirname(__file__))
         self.data_dir = self.tests_dir.parent / 'data'
         self.test_data_dir = self.tests_dir / 'data'
 
-    def test_year(self):
-        """Test for year"""
+@pytest.fixture(scope="session")
+def setup():
+    """General setup options"""
+    return Settings()
 
-        year = 2017
-        playoff_round = 1
-        directory = self.test_data_dir
-        dfile = DataFile(year=year, playoff_round=playoff_round, directory=directory)
+def test_year(setup):
+    """Test for year"""
+    year = 2017
+    playoff_round = 1
+    directory = setup.test_data_dir
+    dfile = DataFile(year=year, playoff_round=playoff_round, directory=directory)
+    assert dfile.year == year
 
-        assert dfile.year == year
+def test_playoff_round(setup):
+    """Test for playoff_round"""
+    year = 2017
+    playoff_round = 1
+    directory = setup.test_data_dir
+    dfile = DataFile(year=year, playoff_round=playoff_round, directory=directory)
+    assert dfile.playoff_round == playoff_round
 
-    def test_playoff_round(self):
-        """Test for playoff_round"""
 
-        year = 2017
-        playoff_round = 1
-        directory = self.test_data_dir
-        dfile = DataFile(year=year, playoff_round=playoff_round, directory=directory)
 
-        assert dfile.playoff_round == playoff_round
+def test_selections_file_input(setup):
+    """Test for selections_file with input"""
+    year = 2017
+    playoff_round = 1
+    directory = setup.test_data_dir
+    dfile = DataFile(year=year, playoff_round=playoff_round, directory=directory)
+    expected_value = directory / f"{year} Deepwell Cup Round {playoff_round}.csv"
+    assert dfile.selections_file == expected_value
 
-    def test_selections_file_input(self):
-        """Test for selections_file with input"""
+def test_selections_file_default(setup):
+    """Test for selections_file with default path"""
+    year = 2017
+    playoff_round = 1
+    dfile = DataFile(year=year, playoff_round=playoff_round)
+    expected_value = setup.data_dir / str(year) / \
+                    f"{year} Deepwell Cup Round {playoff_round}.csv"
+    assert dfile.selections_file == expected_value
 
-        year = 2017
-        playoff_round = 1
-        directory = self.test_data_dir
-        dfile = DataFile(year=year, playoff_round=playoff_round, directory=directory)
-        expected_value = directory / f"{year} Deepwell Cup Round {playoff_round}.csv"
+def test_selections_file_default_champions(setup):
+    """Test for selections_file with default path and champions selections"""
+    year = 2017
+    playoff_round = 'Champions'
+    dfile = DataFile(year=year, playoff_round=playoff_round)
+    expected_value = setup.data_dir / str(year) / \
+                    f"{year} Deepwell Cup Round 1.csv"
+    assert dfile.selections_file == expected_value
 
-        assert dfile.selections_file == expected_value
-
-    def test_selections_file_default(self):
-        """Test for selections_file with default path"""
-
-        year = 2017
-        playoff_round = 1
-        dfile = DataFile(year=year, playoff_round=playoff_round)
-        expected_value = self.data_dir / str(year) / \
-                        f"{year} Deepwell Cup Round {playoff_round}.csv"
-
-        assert dfile.selections_file == expected_value
-
-    def test_selections_file_default_champions(self):
-        """Test for selections_file with default path and champions selections"""
-
-        year = 2017
-        playoff_round = 'Champions'
-        dfile = DataFile(year=year, playoff_round=playoff_round)
-        expected_value = self.data_dir / str(year) / \
-                        f"{year} Deepwell Cup Round 1.csv"
-
-        assert dfile.selections_file == expected_value
-
-    def test_other_points_file_default(self):
-        """Test for other_points_file with default path"""
-
-        year = 2017
-        playoff_round = 1
-        dfile = DataFile(year=year, playoff_round=playoff_round)
-        expected_value = self.data_dir / str(year) / \
-                        f"{year} Deepwell Cup Other Points Round {playoff_round}.csv"
-
-        assert dfile.other_points_file == expected_value
+def test_other_points_file_default(setup):
+    """Test for other_points_file with default path"""
+    year = 2017
+    playoff_round = 1
+    dfile = DataFile(year=year, playoff_round=playoff_round)
+    expected_value = setup.data_dir / str(year) / \
+                    f"{year} Deepwell Cup Other Points Round {playoff_round}.csv"
+    assert dfile.other_points_file == expected_value
