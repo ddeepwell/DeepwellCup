aabcedc05ed8b2929393f4cd9b2f5187dc2d3bd4
David Deepwell <ddeepwel@uwaterloo.ca>
2023-01-21 21:21:23 -0700

    Add scripts to update selections or results

---
+++ scripts/update_results.py
@@ -0,0 +1,44 @@
+"""Populate the database with the results and make the standings plot
+for a specific playoff round"""
+import argparse
+from scripts.playoff_round import PlayoffRound
+
+def update_results(year, playoff_round, **kwargs):
+    """Add results to database and make new stanings plot"""
+
+    current_round = PlayoffRound(
+        year=year,
+        playoff_round=playoff_round,
+        **kwargs
+    )
+    current_round.add_results_to_database()
+    current_round.make_standings_chart()
+
+def main():
+    """Main argument processing"""
+
+    parser = argparse.ArgumentParser(description = 'Import data into database')
+    # required arguments
+    required = parser.add_argument_group('required arguments')
+    required.add_argument("-y", "--year",
+                            type=int,
+                            help = "Year to update",
+                            required = True)
+    required.add_argument("-r", "--playoff_round",
+                            type=int,
+                            help = "Playoff round to update",
+                            required = True)
+    # optional arguments
+    parser.add_argument("-d", "--database",
+                            type=str,
+                            help = "Database to import data into")
+    # parse the arguments
+    args = parser.parse_args()
+
+    # parse the database
+    database = {} if args.database is None else {'database': args.database}
+
+    update_results(args.year, args.playoff_round, **database)
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/update_selections.py
@@ -0,0 +1,44 @@
+"""Populate the database and make the selections table for a specific playoff round"""
+import argparse
+from scripts.playoff_round import PlayoffRound
+
+def update_selections(year, playoff_round, **kwargs):
+    """Update the database and selections table"""
+
+    current_round = PlayoffRound(
+        year=year,
+        playoff_round=playoff_round,
+        **kwargs
+    )
+    current_round.add_selections_to_database()
+    current_round.add_other_points_to_database()
+    current_round.make_latex_table()
+
+def main():
+    """Main argument processing"""
+
+    parser = argparse.ArgumentParser(description = 'Import data into database')
+    # required arguments
+    required = parser.add_argument_group('required arguments')
+    required.add_argument("-y", "--year",
+                            type=int,
+                            help = "Year to update",
+                            required = True)
+    required.add_argument("-r", "--playoff_round",
+                            type=int,
+                            help = "Playoff round to update",
+                            required = True)
+    # optional arguments
+    parser.add_argument("-d", "--database",
+                            type=str,
+                            help = "Database to import data into")
+    # parse the arguments
+    args = parser.parse_args()
+
+    # parse the database
+    database = {} if args.database is None else {'database': args.database}
+
+    update_selections(args.year, args.playoff_round, **database)
+
+if __name__ == "__main__":
+    main()
