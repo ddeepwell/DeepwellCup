db906a06b25aa85f0880c75dac3ecc6fa85d3f83
David Deepwell <ddeepwel@uwaterloo.ca>
2023-02-01 22:21:01 -0700

    Allow non-unique years in Overtime Results table

--- database/add_OvertimeResults_table.sh
+++ database/add_OvertimeResults_table.sh
@@ -4,7 +4,7 @@ db_file=$1
 
 sqlite3 $db_file \
 "CREATE TABLE OvertimeResults (
-    Year            INTEGER (4) UNIQUE NOT NULL,
+    Year            INTEGER (4) NOT NULL,
     Round           INTEGER (1) NOT NULL,
     Overtime          INTEGER NOT NULL,
     PRIMARY KEY ( Year, Round )
