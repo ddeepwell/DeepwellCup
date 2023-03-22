385849851749841e660ce6aa25a57e41d9be0b71
David Deepwell <ddeepwel@uwaterloo.ca>
2023-03-22 12:28:36 -0600

    Clear contents of scripts/__init__.py

--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-''' deepcup
-   This module contains the functions for
-   inserting and extracting information from the database,
-   and for creating tables, graphs and statistics
-   for the annual Deepwell Cup'''
-
-__author__ = "David Deepwell"
-__date__   = "December, 2022"
