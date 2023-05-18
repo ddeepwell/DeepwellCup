42edf3205d5b54f28a8a35d6ab0bde82bb608bcc
David Deepwell <ddeepwel@uwaterloo.ca>
2023-05-17 20:50:42 -0600

    Add script to only create standings plot

--- src/deepwellcup/processing/update_results.py
+++ src/deepwellcup/processing/update_results.py
@@ -4,7 +4,7 @@ import argparse
 from deepwellcup.processing import utils
 from deepwellcup.processing.playoff_round import PlayoffRound
 
-def update_results(year, playoff_round, **kwargs):
+def update_results(year, playoff_round, update_database=True, **kwargs):
     """Add results to database and make new stanings plot"""
 
     current_round = PlayoffRound(
@@ -12,12 +12,36 @@ def update_results(year, playoff_round, **kwargs):
         playoff_round=playoff_round,
         **kwargs
     )
-    current_round.add_results_to_database()
+    if update_database:
+        current_round.add_results_to_database()
     current_round.make_standings_chart()
+
+def main_without_database():
+    """Main function with default to not update database"""
+
+    parser = parse_arguments()
+    args = parser.parse_args()
+    args = modify_and_check_arguments(args)
+    database = {} if args.database is None else {'database': args.database}
+
+    update_results(args.year, args.playoff_round, update_database=False, **database)
 
 def main():
     """Main argument processing"""
 
+    parser = parse_arguments()
+    parser.add_argument("-n", "--no-database-update",
+                        action="store_true",
+                        help = "If used, do not add data to database")
+    args = parser.parse_args()
+    args = modify_and_check_arguments(args)
+    update_database = not args.no_database_update
+    database = {} if args.database is None else {'database': args.database}
+
+    update_results(args.year, args.playoff_round, update_database, **database)
+
+def parse_arguments():
+    """Argument parsing"""
     parser = argparse.ArgumentParser(description = 'Import data into database')
     # required arguments
     required = parser.add_argument_group('required arguments')
@@ -32,18 +56,16 @@ def main():
     parser.add_argument("-d", "--database",
                             type=str,
                             help = "Database to import data into")
-    # parse the arguments
-    args = parser.parse_args()
+    return parser
+
+def modify_and_check_arguments(args):
+    """Modify arguments"""
     if args.playoff_round.isdigit():
         args.playoff_round = int(args.playoff_round)
     selection_rounds = utils.selection_rounds(args.year)
     if args.playoff_round not in selection_rounds:
         raise Exception(f'The playoff round must be one of {selection_rounds}')
-
-    # parse the database
-    database = {} if args.database is None else {'database': args.database}
-
-    update_results(args.year, args.playoff_round, **database)
+    return args
 
 if __name__ == "__main__":
     main()
