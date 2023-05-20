2891d5a4bb60c444f0b7697e8d1e822e63a21364
David Deepwell <ddeepwel@uwaterloo.ca>
2023-05-20 16:32:16 -0600

    Update database initialization script

--- src/deepwellcup/database/init_database.sh
+++ src/deepwellcup/database/init_database.sh
@@ -10,14 +10,7 @@ else
 fi
 
 DATABASE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'Individuals.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'StanleyCupSelections.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'StanleyCupResults.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'Series.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'SeriesSelections.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'SeriesResults.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'OtherPoints.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'OvertimeSelections.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'OvertimeResults.txt'
-$DATABASE_DIR/add_table.sh $db_file $DATABASE_DIR/'Nicknames.txt'
+table_files=($(ls $DATABASE_DIR/*.txt))
+for file in "${table_files[@]}"; do
+    $DATABASE_DIR/add_table.sh $db_file $file
+done
