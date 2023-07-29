ad7ac3fbb79d1ad2fb5a94cbcb80c9d11b2a327e
David Deepwell <dsdeepwell@gmail.com>
2023-07-28 22:21:56 -0600

    Move year parsing in remake away from CLI argument parsing
    Clean up notation and add type hints, as well.

--- src/deepwellcup/processing/remake.py
+++ src/deepwellcup/processing/remake.py
@@ -1,16 +1,19 @@
-"""Script to populate and remake all figures and tables for a range of years"""
+"""Remake everything."""
 import argparse
-from deepwellcup.processing.playoff_round import PlayoffRound
+from .playoff_round import PlayoffRound
 
 
-def multi_year_remake(years, **kwargs):
-    """Remake the entire database and all figures and tables between year1 and year2"""
-    for year in years:
+def multi_year_remake(
+    years: int | list[int],
+    database: str | None = None,
+) -> None:
+    """Remake the database, figures and tables."""
+    for year in _parse_year_inputs(years):
         for rnd in [1, 2, 3, 4]:
             current_round = PlayoffRound(
                 year=year,
                 playoff_round=rnd,
-                **kwargs
+                database=database,
             )
             current_round.add_selections_to_database()
             current_round.add_other_points_to_database()
@@ -19,37 +22,47 @@ def multi_year_remake(years, **kwargs):
             current_round.make_standings_chart()
 
 
-def main():
-    """Main argument processing"""
-    parser = argparse.ArgumentParser(description='Import data into database')
-    # required arguments
+def _parse_year_inputs(input_years: int | list[int]) -> list:
+    """Return the list of years to remake from:
+    1) the final year (ie, start from the beginning)
+    2) the first and final years"""
+    very_first_year = 2006
+    if isinstance(input_years, int):
+        return range(very_first_year, input_years+1)
+    num_years = len(input_years)
+    if num_years not in [1, 2]:
+        raise ValueError(
+            'The years argument must be of length 1 or 2. '
+            f'It was {len(input_years)}'
+        )
+    if num_years == 1:
+        return range(very_first_year, input_years[0]+1)
+    return range(input_years[0], input_years[1]+1)
+
+
+def main() -> None:
+    """Command line argument processing"""
+    parser = argparse.ArgumentParser(
+        description='Remake the database, figures and tables'
+    )
     required = parser.add_argument_group('required arguments')
     required.add_argument(
         "-y", "--years",
         nargs='+',
         type=int,
-        help="Years to remake",
-        required=True
+        help="year extrema to remake",
+        required=True,
     )
-    # optional arguments
     parser.add_argument(
         "-d", "--database",
         type=str,
-        help="Database to import data into"
+        help="database to import data into",
     )
-    # parse the arguments
     args = parser.parse_args()
-
-    # decide which years to remake
-    year_args = getattr(args, 'years')
-    year1 = 2006 if len(year_args) == 1 else year_args[0]
-    year2 = year_args[-1]
-    years = range(year1, year2+1)
-
-    # parse the database
-    database = {} if args.database is None else {'database': args.database}
-
-    multi_year_remake(years, **database)
+    multi_year_remake(
+        years=args.years,
+        database=args.database,
+    )
 
 
 if __name__ == "__main__":
