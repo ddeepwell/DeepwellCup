eb39ef038c8301c4a983eae803c99b2189625aa8
David Deepwell <dsdeepwell@gmail.com>
2023-09-21 20:08:56 -0600

    Add monikers to ingestion class

--- src/deepwellcup/processing/ingestion.py
+++ src/deepwellcup/processing/ingestion.py
@@ -1,6 +1,8 @@
 """Read participant round selection data from the data files."""
 from pathlib import Path
 
+import pandas as pd
+
 from . import files
 from .utils import RoundsInfo, SelectionRound
 
@@ -20,6 +22,7 @@ class Ingestion():
             selection_round=playoff_round,
             directory=raw_data_directory,
         )
+        self._raw_contents = self.read_file()
 
     @property
     def round_info(self) -> RoundsInfo:
@@ -30,3 +33,33 @@ class Ingestion():
     def selections_file(self) -> Path:
         """Directory with the raw data."""
         return self._selections_file
+
+    @property
+    def raw_contents(self) -> pd.DataFrame:
+        """Raw file contents."""
+        return self._raw_contents
+
+    def read_file(self) -> pd.DataFrame:
+        """Read selections from selections file."""
+        contents = pd.read_csv(
+            self.selections_file,
+            sep=',',
+            converters={
+                'Name:': str.strip,
+                'Moniker': str.strip,
+            }
+        )
+        return contents.rename(columns={'Name:': 'Individual'})
+
+    def monikers(self) -> dict[str, str] | None:
+        """Extract monikers."""
+        if 'Moniker' in self.raw_contents.columns:
+            return (
+                self.raw_contents[['Individual', 'Moniker']]
+                .set_index('Individual')
+                .drop(labels='Results', axis='index')
+                .squeeze()
+                .sort_index()
+                .to_dict()
+            )
+        return None
---
+++ tests/test_ingestion.py
@@ -0,0 +1,14 @@
+"""Tests for Ingestion class"""
+import pytest
+
+from deepwellcup.processing.ingestion import Ingestion
+
+
+def test_monikers():
+    """Test for monikers."""
+    ing = Ingestion(
+        year=2018,
+        playoff_round=4,
+        raw_data_directory=pytest.test_data_dir,
+    )
+    assert ing.monikers() == {"Alita D": "", "David D": "Nazzy"}
