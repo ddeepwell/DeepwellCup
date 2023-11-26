bf6fffe64d8dd6a79b593459f120726c6600ad2b
David Deepwell <dsdeepwell@gmail.com>
2023-11-25 19:11:06 -0700

    Convert PlayoffRound to dataclass

--- src/deepwellcup/processing/playoff_round.py
+++ src/deepwellcup/processing/playoff_round.py
@@ -1,53 +1,45 @@
-"""Hold all data for a playoff round in a year"""
+"""Hold all data for a playoff round in a year."""
+from dataclasses import dataclass
+
+from .database_new import DataBase
 from .points import RoundPoints
 from .round_data import RoundData
-from .utils import DataStores
+from .utils import SelectionRound
 
 
-class PlayoffRound:  # pylint: disable=R0902
-    """Class for all information about a playoff round"""
+@dataclass
+class PlayoffRound:
+    """Class for all information about a playoff round."""
 
-    def __init__(
-        self,
-        year,
-        playoff_round,
-        datastores: DataStores = DataStores(None, None),
-    ):
-        self.year = year
-        self.playoff_round = playoff_round
-        self._datastores = datastores
-        round_data = RoundData(
-            year, playoff_round, database=datastores.database  # type: ignore[arg-type]
-        )
+    year: int
+    selection_round: SelectionRound
+    database: DataBase
+
+    def __post_init__(self):
+        round_data = RoundData(self.year, self.selection_round, self.database)
         self._points = RoundPoints(round_data)
         self._selections = round_data.selections
         self._results = round_data.results
-        self._other_points = round_data.other_points
 
     @property
     def selections(self):
-        """All selections for the playoff round"""
+        """Return selections for the playoff round."""
         return self._selections
 
     @property
     def results(self):
-        """All results for the playoff round"""
+        """Return results for the playoff round."""
         return self._results
 
     @property
-    def other_points(self):
-        """All other points for the playoff round"""
-        return self._other_points
-
-    @property
     def points(self):
-        """All other points for the playoff round"""
-        return self._points.total
+        """Return other points for the playoff round."""
+        return self._points
 
     @property
     def individuals(self):
         """Return the individuals in the round."""
-        if self.playoff_round == "Champions":
+        if self.selection_round == "Champions":
             selections = self.selections.champions
         else:
             selections = self.selections.series
@@ -55,14 +47,14 @@ class PlayoffRound:  # pylint: disable=R0902
             selections.index.get_level_values("Individual").unique()
         )
         other_players = set(
-            self.other_points.points.index.get_level_values("Individual").unique()
+            self.points.other.index.get_level_values("Individual").unique()
         )
         return list(selection_players.union(other_players))
 
     @property
     def series(self):
-        """The series in the playoff round"""
-        if self.playoff_round == "Champions":
+        """Return the series in the playoff round."""
+        if self.selection_round == "Champions":
             return {}
         conferences = list(
             set(self.results.series.index.get_level_values("Conference"))
